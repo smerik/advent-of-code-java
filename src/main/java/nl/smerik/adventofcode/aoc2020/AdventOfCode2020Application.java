0500fff71765e34fdca88fe7fdb949532ae55986
Erik van der Kolk <developer@smerik.nl>
2020-12-01 21:37:35 +0100

    Initial project setup.
    * Add Spring Boot application class.
    * Add Spring Boot configuration
    * Add Project Lombok configuration
    * Java 15 application

---
+++ src/main/java/nl/smerik/adventofcode/aoc2020/AdventOfCode2020Application.java
@@ -0,0 +1,26 @@
+package nl.smerik.adventofcode.aoc2020;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@Slf4j
+@SpringBootApplication
+public class AdventOfCode2020Application implements CommandLineRunner {
+
+    public static void main(String[] args) {
+        LOG.info("Starting AdventOfCode2020Application...");
+        SpringApplication.run(AdventOfCode2020Application.class, args);
+        LOG.info("Finished AdventOfCode2020Application.");
+    }
+
+    @Override
+    public void run(final String... args) throws Exception {
+        LOG.info("Executing CommandLineRunner...");
+
+        for (int i = 0; i < args.length; ++i) {
+            LOG.info("args[{}]: {}", i, args[i]);
+        }
+    }
+}
