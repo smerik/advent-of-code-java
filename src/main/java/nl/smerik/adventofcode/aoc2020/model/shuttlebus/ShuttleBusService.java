9d2d96abac5c8154f73ec812b993d6b2f143d61f
Erik van der Kolk <developer@smerik.nl>
2021-09-20 21:59:43 +0200

    Day 13 - part 02: Additional documentation
    Provide the implementation with the explanation of reddit user
    `imbadatreading`.

--- src/main/java/nl/smerik/adventofcode/aoc2020/model/shuttlebus/ShuttleBusService.java
+++ src/main/java/nl/smerik/adventofcode/aoc2020/model/shuttlebus/ShuttleBusService.java
@@ -36,12 +36,30 @@ public class ShuttleBusService {
      * Finds the earliest timestamp such that the first bus ID departs at that time and each subsequent listed bus ID
      * departs at that subsequent minute.
      * <p>
+     * Although my implementation can be done neater, it is a working one.
+     * <p>
      * This implementation is based on the Python
      * <a href="https://www.reddit.com/r/adventofcode/comments/kc4njx/2020_day_13_solutions/gfth69h/?utm_source=reddit&utm_medium=web2x&context=3">solution</a>
      * description of reddit user
      * <a href="https://www.reddit.com/user/imbadatreading/">imbadatreading</a>.
      * <p>
-     * Although the implementation can be done neater, it is a working one.
+     * His thought process was that he needed to find a number such that it was going to be divisible by the first bus,
+     * and then divisible by the second bus PLUS its index, and so on.
+     * So in the sample <code>7,13,x,x,59,x,31,19</code> if you take the first two buses,
+     * we need a number divisible by <code>7</code> that when you add <code>1</code> to it
+     * (the index of bus <code>13</code>) the number becomes divisible by <code>13</code>.
+     * This happens at <code>77</code>, because <code>77 + 1 = 78</code> which is divisible by <code>13</code>.
+     * The next number we need to find is a number that is divisible by
+     * <code>7</code>, <code>13 + 1</code> and <code>59 + 4</code>.
+     * This continues for all the buses.
+     * <p>
+     * Obviously the final number is going to be huge, so you need a way to generate candidates,
+     * and this happens with the realisation that the buses are all prime numbers:
+     * you can take their product and keep adding it until it meets your conditions.
+     * For example the least common multiple (lcm) of <code>7</code> and <code>13</code> is <code>91</code>,
+     * so we keep adding <code>91</code> to <code>77</code> until we find a number that aligns with
+     * <code>7, 13+1, 59+4</code>.
+     * Then the LCM becomes <code>7 * 13 * 59</code>, and this continues until you've found a number for all your buses.
      *
      * @param initialTimestamp the base time stamp to start finding the solution
      * @return the earliest timestamp
