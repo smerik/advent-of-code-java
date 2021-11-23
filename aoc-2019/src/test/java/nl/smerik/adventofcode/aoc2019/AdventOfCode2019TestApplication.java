f1d30ffaf51ba8c76b67a08099dcf0403f939f1d
Erik van der Kolk <developer@smerik.nl>
2021-11-23 19:31:17 +0100

    Make project build with migrated module `aoc-2019`
    * Add dependency management in root `pom.xml`
    * Fix `pom.xml` for `aoc-2019`
    * Add name tag for `aoc-2020`

--- aoc-2019/src/test/java/nl/smerik/adventofcode/aoc2019/AdventOfCode2019TestApplication.java
+++ aoc-2019/src/test/java/nl/smerik/adventofcode/aoc2019/AdventOfCode2019TestApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class AdventOfCode2019Application {
+public class AdventOfCode2019TestApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(AdventOfCode2019Application.class, args);
+        SpringApplication.run(AdventOfCode2019TestApplication.class, args);
     }
 }
