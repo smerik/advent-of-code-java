3594d1ce6264702b57a96b3c7a2ef4a87d922db4
Erik van der Kolk <developer@smerik.nl>
2021-11-25 15:04:58 +0100

    Refactor day services to use PuzzleInputParser.parseToLong()`
    Change the day service classes implementation using long input
    so that they make use of the puzzle input parser utility class.

--- aoc-2020/src/main/java/nl/smerik/adventofcode/aoc2020/day/Day09Service.java
+++ aoc-2020/src/main/java/nl/smerik/adventofcode/aoc2020/day/Day09Service.java
@@ -1,20 +1,13 @@
 package nl.smerik.adventofcode.aoc2020.day;
 
-import lombok.SneakyThrows;
-import lombok.extern.slf4j.Slf4j;
 import nl.smerik.adventofcode.aoc2020.service.crypto.ExchangeMaskingAdditionSystemService;
+import nl.smerik.adventofcode.io.PuzzleInputParser;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Service;
 
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.List;
-import java.util.stream.Stream;
 
-@Slf4j
 @Service
 public class Day09Service {
 
@@ -27,29 +20,15 @@ public class Day09Service {
         this.xmasService = xmasService;
     }
 
-    @SneakyThrows
     public Long getSolutionPart1() {
-        final Path path = Paths.get(resource.getURI());
-        try (Stream<String> stringStream = Files.lines(path)) {
-            final List<Long> input = stringStream.mapToLong(Long::valueOf).boxed().toList();
-            return xmasService.validate(input, 25);
-        } catch (IOException e) {
-            LOG.error("Houston: {}", e.getMessage(), e);
-            return null;
-        }
+        final List<Long> input = PuzzleInputParser.parseToLong(resource);
+        return xmasService.validate(input, 25);
     }
 
-    @SneakyThrows
     public Long getSolutionPart2() {
-        final Path path = Paths.get(resource.getURI());
-        try (Stream<String> stringStream = Files.lines(path)) {
-            final List<Long> input = stringStream.mapToLong(Long::valueOf).boxed().toList();
-            final Long invalidSum = xmasService.validate(input, 25);
-            final List<Long> contiguousRange = xmasService.findContiguousRange(input, invalidSum);
-            return xmasService.findEncryptionWeakness(contiguousRange);
-        } catch (IOException e) {
-            LOG.error("Houston: {}", e.getMessage(), e);
-            return null;
-        }
+        final List<Long> input = PuzzleInputParser.parseToLong(resource);
+        final Long invalidSum = xmasService.validate(input, 25);
+        final List<Long> contiguousRange = xmasService.findContiguousRange(input, invalidSum);
+        return xmasService.findEncryptionWeakness(contiguousRange);
     }
 }
