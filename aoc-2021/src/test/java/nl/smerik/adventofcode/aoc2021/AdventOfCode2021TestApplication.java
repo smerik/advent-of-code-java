c97ca3672c855947296df47fa0eb3594f240f1ba
Erik van der Kolk <developer@smerik.nl>
2021-11-29 21:01:29 +0100

    Make project ready for AoC 2021

---
+++ aoc-2021/src/test/java/nl/smerik/adventofcode/aoc2021/AdventOfCode2021TestApplication.java
@@ -0,0 +1,14 @@
+package nl.smerik.adventofcode.aoc2021;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@Slf4j
+@SpringBootApplication
+public class AdventOfCode2021TestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdventOfCode2021TestApplication.class, args);
+    }
+}
